a4c738460858c421e5b3d6a75c08f3b44830e1df
Vedran Omeragic <77688940+vedran-omeragic@users.noreply.github.com>
2022-11-21 00:39:26 +0100

    Adding user form to create and update users

--- src/features/user/models.ts
+++ src/features/user/models.ts
@@ -1,13 +1,23 @@
 import { UserPermission } from '../userPermissons/models';
 
-export interface User {
-	id: number;
+export interface UserData {
 	username: string;
 	email: string;
 	first_name: string | null;
 	last_name: string | null;
 	date_of_birth: Date | null;
 	status: string | null;
+}
+
+export interface UserFormData extends UserData {
+	password: string;
+}
+
+export interface UserFormUpdateData extends UserFormData {
+	id: number;
+}
+export interface User extends UserData {
+	id: number;
 	created_at: Date;
 	updated_at: Date | null;
 	user_permissions: UserPermission[];
---
+++ src/pages/UserFormPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserForm from '../components/form/UserForm';
+
+const UserFormPage = () => {
+	return (
+		<div className="page">
+			<UserForm />
+		</div>
+	);
+};
+
+export default UserFormPage;
